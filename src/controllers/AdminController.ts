10da874fa1b8132017d56a5c78f167191e449196
aomkhamphaung <akhamphaung@gmail.com>
2024-03-29 13:43:20 +0630

    refactor: find vandor with mail or id

--- src/controllers/AdminController.ts
+++ src/controllers/AdminController.ts
@@ -3,6 +3,14 @@ import { CreateVandorInput } from "../dto";
 import { Vandor } from "../models";
 import { GeneratePassword, GenerateSalt } from "../utility";
 
+export const FindVandor = async (id: string | undefined, email?: string) => {
+  if (email) {
+    return await Vandor.findOne({ email: email });
+  } else {
+    return await Vandor.findById(id);
+  }
+};
+
 export const CreateVandor = async (
   req: Request,
   res: Response,
@@ -19,7 +27,7 @@ export const CreateVandor = async (
     password,
   } = <CreateVandorInput>req.body;
 
-  const existingVandor = await Vandor.findOne({ email: email });
+  const existingVandor = await FindVandor("", email);
 
   if (existingVandor !== null) {
     return res
@@ -68,7 +76,7 @@ export const GetVandorById = async (
   next: NextFunction
 ) => {
   const vandorId = req.params.id;
-  const vandor = await Vandor.findById(vandorId);
+  const vandor = await FindVandor(vandorId);
 
   if (vandor !== null) {
     return res.status(200).json(vandor);
