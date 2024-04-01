b8abe7ec2f618cb9d79bbc57f34135622d04f3ba
aomkhamphaung <akhamphaung@gmail.com>
2024-04-01 10:56:51 +0630

    shopping: get food availity

--- src/controllers/ShoppingController.ts
+++ src/controllers/ShoppingController.ts
@@ -1,11 +1,27 @@
 import express, { Request, Response, NextFunction } from "express";
+import { Vandor } from "../models";
 
 /**-------------------- Food Availability -------------------- */
 export const GetFoodAvaility = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {};
+) => {
+  const pincode = req.params.pincode;
+
+  const result = await Vandor.find({
+    pincode: pincode,
+    serviceAvailable: true,
+  })
+    .sort([["rating", "descending"]])
+    .populate("foods");
+
+  if (result.length > 0) {
+    return res.status(200).json(result);
+  }
+
+  return res.status(404).json({ message: "No Data Found!" });
+};
 
 /**-------------------- Top Restaurants -------------------- */
 export const getTopRestaurants = async (
