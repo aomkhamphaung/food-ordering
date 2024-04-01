a89a25509353602ce4ed6f9387696f3ac96db5c4
aomkhamphaung <akhamphaung@gmail.com>
2024-04-01 15:48:58 +0630

    wip: customer signup with otp

--- src/controllers/CustomerController.ts
+++ src/controllers/CustomerController.ts
@@ -1,11 +1,51 @@
 import express, { Request, Response, NextFunction } from "express";
+import { plainToClass } from "class-transformer";
+import { CreateCustomerInput } from "../dto";
+import { validate } from "class-validator";
+import { GenerateOtp, GeneratePassword, GenerateSalt } from "../utility";
+import { Customer } from "../models/Customer";
 
 /**-------------------- Signup -------------------- */
 export const customerSignup = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {};
+) => {
+  const CustomerInputs = plainToClass(CreateCustomerInput, req.body);
+
+  const inputErrors = await validate(CustomerInputs, {
+    validationError: { target: true },
+  });
+
+  if (inputErrors.length > 0) {
+    return res.status(400).json(inputErrors);
+  }
+
+  const { email, phone, password } = CustomerInputs;
+
+  const salt = await GenerateSalt();
+  const customerPassword = await GeneratePassword(password, salt);
+
+  const { otp, expiry } = await GenerateOtp();
+
+  const result = await Customer.create({
+    email: email,
+    password: customerPassword,
+    salt: salt,
+    phone: phone,
+    otp: otp,
+    otp_expiry: expiry,
+    firstName: "",
+    lastName: "",
+    address: "",
+    verified: false,
+    lat: 0,
+    lng: 0,
+  });
+
+  if (result) {
+  }
+};
 
 /**-------------------- Login -------------------- */
 export const customerLogin = async (
---
+++ src/utility/NotificationUtility.ts
@@ -0,0 +1,7 @@
+export const GenerateOtp = async () => {
+  const otp = Math.floor(100000 + Math.random() * 900000);
+  let expiry = new Date();
+  expiry.setTime(new Date().getTime() + 30 * 60 * 1000);
+
+  return { otp, expiry };
+};
